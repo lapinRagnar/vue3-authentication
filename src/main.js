5ac7a726a077c40af73a09f663a2261ee7129fee
lapinRagnar <lapinragnar@gmail.com>
2022-10-18 18:04:29 +0200

    2 - installation de bootstrap-vue-3

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue-3'
 
-createApp(App).use(store).use(router).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+createApp(App).use(BootstrapVue).use(IconsPlugin).use(store).use(router).mount('#app')
